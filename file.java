1b62fa1e73b549513ba58206ba81e1bc71b3025e
janhavi1802 <138413043+janhavi1802@users.noreply.github.com>
2024-12-07 19:00:21 +0530

    Update file.java

--- file.java
+++ file.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class file {
     public static void main(String[] args) {
         // Print a simple message to the console
         System.out.println("Hello, World!");
